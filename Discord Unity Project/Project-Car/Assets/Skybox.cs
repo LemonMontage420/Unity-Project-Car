323bb187a9f80d1ade4632e701c0374b8e4a2921
LemonMontage420 <82865091+LemonMontage420@users.noreply.github.com>
2021-04-20 17:29:26 +0200

    More Progress On Skybox2

---
+++ Discord Unity Project/Project-Car/Assets/Skybox.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Skybox : MonoBehaviour
+{
+    public Material skyboxMaterial;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (skyboxMaterial.GetFloat("_RadiusB") >= skyboxMaterial.GetFloat("_RadiusA"))
+        {
+            skyboxMaterial.SetFloat(("_RadiusB"), skyboxMaterial.GetFloat("_RadiusA") - 0.001f);
+        }
+    }
+}
